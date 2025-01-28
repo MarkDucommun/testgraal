85a074296fc47ecf5f8942c34270d9741276c756
Mark Ducommun <ducommun.mark@gmail.com>
2025-01-27 20:36:40 -0600

    more

--- terraform-azure-executable/main.tf
+++ terraform-azure-executable/main.tf
@@ -5,32 +5,32 @@ resource "azurerm_resource_group" "testgraal_rg" {
 
 resource "azurerm_storage_account" "testgraal_storage" {
   name                     = "storagetestgraal"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
+  resource_group_name      = azurerm_resource_group.testgraal_rg.name
+  location                 = azurerm_resource_group.testgraal_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_service_plan" "testgraal_service_plan" {
   name                = "asp-functionapp-testgraal"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = azurerm_resource_group.testgraal_rg.name
+  location            = azurerm_resource_group.testgraal_rg.location
   os_type             = "Linux"
   sku_name = "Y1"
 }
 
 resource "azurerm_application_insights" "app_insights" {
   name                = "${var.function_app_name}-appinsights"
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.testgraal_rg.location
+  resource_group_name = azurerm_resource_group.testgraal_rg.name
   application_type    = "web" # Application Insights type for web apps and function apps
 }
 
 
 resource "azurerm_linux_function_app" "functionapp" {
   name                = var.function_app_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = azurerm_resource_group.testgraal_rg.name
+  location            = azurerm_resource_group.testgraal_rg.location
   service_plan_id     = azurerm_service_plan.testgraal_service_plan.id
   storage_account_name       = azurerm_storage_account.testgraal_storage.name
   storage_account_access_key = azurerm_storage_account.testgraal_storage.primary_access_key
